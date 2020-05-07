accde8ba318a83c31fadac594a2dc38fe7f919f9
Liu Chang <luroylau@163.com>
2020-05-07 11:29:22 +0800

    setup test environment

--- test/source/test_cartesian.cpp
+++ test/source/test_cartesian.cpp
@@ -1,1 +1,13 @@
+#include "cartesian.hpp"
 #include "catch.hpp"
+
+namespace cosphy {
+
+TEST_CASE("Cartesian constructor", "[cosphy Cartesian]") {
+    Cartesian origin;
+    REQUIRE(origin[0] == 0.0);
+    REQUIRE(origin[1] == 0.0);
+    REQUIRE(origin[2] == 0.0);
+}
+
+}  // namespace cosphy

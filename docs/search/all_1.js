88e6b9db69d208bf1ec4b9c4eb00463effc0adc3
Liu Chang <luroylau@163.com>
2020-05-08 09:45:15 +0800

    remove docs into standalone branch

--- docs/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sperical_1',['Sperical',['../classcosphy_1_1_sperical.html',1,'cosphy']]]
-];
--- docs/search/classes_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sperical_3',['Sperical',['../classcosphy_1_1_sperical.html',1,'cosphy']]]
-];
